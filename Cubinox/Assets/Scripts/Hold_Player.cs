7c6e91dae815443832e35ee897c8c02284fb9017
mckano <mckano54@gmail.com>
2019-06-11 19:50:09 -0500

    saving

---
+++ Cubinox/Assets/Scripts/Hold_Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hold_Player : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player_Motor pm = collision.gameObject.GetComponent<Player_Motor>();
+
+        if(pm != null)
+        {
+            pm.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Player_Motor pm = collision.gameObject.GetComponent<Player_Motor>();
+
+        if (pm != null)
+        {
+            pm.transform.parent = null;
+        }
+    }
+}
