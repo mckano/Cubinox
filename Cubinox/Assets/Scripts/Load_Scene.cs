284b976680093b0c5f0dfcd3de2d62bb7eb6c746
mckano <mckano54@gmail.com>
2019-06-13 02:22:25 -0500

    Load Level
    Working on the loading level script

---
+++ Cubinox/Assets/Scripts/Load_Scene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load_Scene : MonoBehaviour
+{
+
+    public string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Player_Motor>())
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
